9d7b17e89ad68c34239e8667416329993a5ecfc6
HOP <mjga2025ardoino@gmail.com>
2026-01-16 21:32:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
         recensioni: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR0aLcG9mPmNexSlEt-5MaNlpIPUF7LPvdlsJwDhoa_1PZfsvFbw9eQu4uBpsrqwTng9TrkqvxcoQRm/pub?gid=534419137&single=true&output=csv",
         ordini: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR0aLcG9mPmNexSlEt-5MaNlpIPUF7LPvdlsJwDhoa_1PZfsvFbw9eQu4uBpsrqwTng9TrkqvxcoQRm/pub?gid=1618872676&single=true&output=csv",
         clienti: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR0aLcG9mPmNexSlEt-5MaNlpIPUF7LPvdlsJwDhoa_1PZfsvFbw9eQu4uBpsrqwTng9TrkqvxcoQRm/pub?gid=1159642077&single=true&output=csv",
-        script_exec: "https://script.google.com/macros/s/AKfycbxRRpm4b5KVsAGIrcZulzqgmAGuU3cG8r-5p1DoCdrM87w4kCLCeDsNZD1WSAq5TdrA/exec"
+        script_exec: "https://script.google.com/macros/s/AKfycby8AEfZJxPnidFPBMx5YrjePNIUnJaA2eHIvEBO8TgpRoeP7l9S9ONd_BcMvphZAAM9/exec"
     },
     contatti: {
         wa: "393358060715",
